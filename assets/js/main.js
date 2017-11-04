835045415c941f69e380cf3d1c2a3259909df13e
Pedro Marinelli <pedromarinelli@outlook.com>
2017-11-04 07:32:38 -0200

    Setup Inicial
    Setup Inicial e Testes.
    Ajuste do arquivo bennu.json faltando um chave de fechamento.

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+//	GERAL ONLOAD
+(function() {
+	console.log("FOI");
+	$.getJSON('assets/js/bennu.json', function (data) {
+		console.log(data);
+		console.log("...ATÉ AQUI");
+	});
+})();
+// Fix para o hover no iOS mobile
+	// function isMobile() {
+	// 	var index = navigator.appVersion.indexOf("Mobile");
+	// 	return (index > -1);
+	// }
+	
+$(document).ready(function() {
+//	GERAL READY	
+
+});
